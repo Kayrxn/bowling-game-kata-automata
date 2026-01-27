c0168732000464af71ba11596b058057e9790bf6
Kayrxn <xharukax99@gmail.com>
2026-01-27 19:15:41 +0100

    feat(automaton.py): Añadir función set_input

--- src/automaton.py
+++ src/automaton.py
@@ -1,3 +1,6 @@
 class Automaton:
     def __init__(self):
         self.rolls = []
+
+def set_input(self, score_card):
+        self.rolls = score_card.rolls  #recibe el objeto scoreCard y extrae los lanzamientos que ya procesó
