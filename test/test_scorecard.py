1217450b50784f4f2e0b106e929a7a3b5fb049ca
Kayrxn <xharukax99@gmail.com>
2026-02-05 09:02:01 +0100

    test(test_scorecard.py): Ampliar cantidad de test

--- test/test_scorecard.py
+++ test/test_scorecard.py
@@ -1,16 +1,100 @@
 import pytest
 from src.ScoreCard import ScoreCard
 
-cases = [
-    pytest.param("12345123451234512345", 60, marks=[pytest.mark.scorecard, pytest.mark.regular], id="regular"),
-    pytest.param("9-9-9-9-9-9-9-9-9-9-", 90, marks=[pytest.mark.scorecard, pytest.mark.zeros], id="zeros"),
-    pytest.param("5/5/5/5/5/5/5/5/5/5/5", 150, marks=[pytest.mark.scorecard, pytest.mark.spare], id="spare"),
-    pytest.param("X9-9-9-9-9-9-9-9-9-", 100, marks=[pytest.mark.scorecard, pytest.mark.strike], id="strike"),
-    pytest.param("XXXXXXXXXXXX", 300, marks=[pytest.mark.scorecard, pytest.mark.perfect], id="perfect"),
-    pytest.param("8/549-XX5/53639/9/X", 149, marks=[pytest.mark.scorecard, pytest.mark.complex], id="complex"),
-]
+@pytest.mark.state_n
+def test_hitting_pins_regular():
+    pins = "12345123451234512345"
+    total = 60
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+@pytest.mark.state_n
+def test_symbol_zero():
+    pins = "9-9-9-9-9-9-9-9-9-9-"
+    total = 90
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+    pins = "9-3561368153258-7181"
+    total = 82
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+@pytest.mark.spare
+def test_spare_not_extra():
+    pins = "9-3/613/815/-/8-7/8-"
+    total = 121
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+@pytest.mark.strike
+def test_strike():
+    # test strike
+    pins = "X9-9-9-9-9-9-9-9-9-"
+    total = 100
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+    pins = "X9-X9-9-9-9-9-9-9-"
+    total = 110
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+@pytest.mark.strike
+def test_two_strikes():
+    pins = "XX9-9-9-9-9-9-9-9-"
+    total = 120
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+@pytest.mark.strike
+def test_three_strikes():
+    pins = "XXX9-9-9-9-9-9-9-"
+    total = 141
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+@pytest.mark.extra_rolls
+def test_one_pin_in_extra_roll():
+    pins = "9-3/613/815/-/8-7/8/8"
+    total = 131
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+    pins = "5/5/5/5/5/5/5/5/5/5/5"
+    total = 150
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+@pytest.mark.extra_rolls
+def test_two_strikes_in_extra_rolls():
+    pins = "9-9-9-9-9-9-9-9-9-XXX"
+    total = 111
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
 
 
-@pytest.mark.parametrize("pins,expected", cases)
-def test_scorecard(pins, expected):
-    assert ScoreCard(pins).score() == expected
+@pytest.mark.extra_rolls
+def test_one_strike_in_extra_roll():
+    pins = "8/549-XX5/53639/9/X"
+    total = 149
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+
+@pytest.mark.extra_rolls
+def test_spare_in_extra_roll():
+    # spare in extra roll
+    pins = "X5/X5/XX5/--5/X5/"
+    total = 175
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
+
+
+@pytest.mark.extra_rolls
+def test_triple_strike_before_extra_rolls():
+    # 12 strikes is a “Thanksgiving Turkey”
+    # 2 strikes in extra rolls
+    pins = "XXXXXXXXXXXX"
+    total = 300
+    score_card = ScoreCard(pins)
+    assert score_card.score() == total
